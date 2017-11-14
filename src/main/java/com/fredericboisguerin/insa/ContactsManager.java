bd15295355d640947b0354c066994512f64b9bd6
mendesfe <damaris.menfer@gmail.com>
2017-11-14 14:35:42 +0100

    Creation of Contact list.

--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,16 +1,35 @@
 package com.fredericboisguerin.insa;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ContactsManager {
 
-    public void addContact(String name, String email, String phoneNumber) {
+    List<Contact> contactList = new ArrayList<Contact>();
 
+    public void addContact(String name, String email, String phoneNumber) {
+        Contact newcontact = new Contact(name,email,phoneNumber);
+        contactList.add(newcontact);
     }
 
     public void printAllContacts() {
+        for (Iterator<Contact> iter = contactList.iterator();iter.hasNext();){
+            iter.next().printContact();
+
+        }
 
     }
 
     public void searchContactByName(String name) {
+        //System.out.printf("%s",name);
+
+        for (Iterator<Contact> iter = contactList.iterator();iter.hasNext();){
+
+            if (iter.next().getName() == name){
+                iter.next().printContact();
+            }
+        }
 
     }
 }
